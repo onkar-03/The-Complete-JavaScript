9c6997bf4a9af35a24230c6774435b02b12b2979
Onkar Patel <onkarpatel10@gmail.com>
2024-03-31 21:14:22 +0530

    Learned about Values & Variables in Js :
    - What are values ??
    - What are Variables ?
    - Rules to Name a Variable in Js
    - Printing Varaibles

---
+++ 01. Fundamentals Part 1/script.js
@@ -0,0 +1,49 @@
+// --------------------------> **************** <---------------------------------
+// ^ Declaring Variable in Js
+let js = 'amazing';
+
+// --------------------------> **************** <---------------------------------
+// ^ Using If statements
+// & alert() used to display the alert Output on the webpage
+// if (js == 'amazing') alert('Js Is Fun !!');
+
+// --------------------------> **************** <---------------------------------
+// ^ console.log()
+// To display results of operations in console we use the console.log();
+console.log(40 + 8 + 2);
+
+// --------------------------> **************** <---------------------------------
+// ^ Values :
+// Smallest unit of Information in Js
+console.log('Onkar');
+console.log(10);
+
+// --------------------------> **************** <---------------------------------
+// ^ Variables :
+/*
+1. 'let' used to declare 'variables'
+ 
+2. Variables store a 'value'
+ 
+3. Declaring Variables using camelCase notation is a common practice
+ 
+4. We can have a letter , number , _ , or $ in the variable name
+ 
+5. Starting with a  Number not allowed
+ 
+6. Using keywords such as 'name' , 'new', 'function' etc... are not allowed as variable names
+ 
+7. Its a convention to never use a var that starts with a Capital letter, they are specially reserved convention to write variables for OOPs in Js
+        eg : 'Person'
+ 
+8. All Uppercase Variables are reserved as constants 
+    eg : 'PI' etc ...
+ 
+9. Write variables names such that they depict what kind of value they hold for us.
+    eg : myFirstJob='programmer' ✅
+         job1 = 'programmer' ❌
+    */
+let firstName = 'Onkar';
+
+// & Printing the value stored in the variable
+console.log(firstName);
