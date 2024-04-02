3c8fcae25b7c2bb6cd4fd491a5ba72ebfaee6360
Onkar Patel <onkarpatel10@gmail.com>
2024-04-02 17:27:06 +0530

    Assignment on Decision Mkaing Done

---
+++ 01. Fundamentals Part 1/Assignemnts/6. Taking Decisions if-else Statements.js
@@ -0,0 +1,26 @@
+// All these variables wont change in the future hence declared them as constants
+const country = 'India';
+const continent = 'Asia';
+const isIsland = false;
+const language = 'Hindi';
+
+// Population changes over time hence it shouldn't be const
+// Population in millions
+let population = 1417;
+
+console.log(population / 2 + ' ' + 'millions');
+population++;
+console.log(population + ' ' + 'millions');
+console.log(population > 6);
+console.log(population < 33);
+
+// Using Template Literals to write off sentences
+const description = `${country} is in ${continent}, and its ${population} million population speaks Hindi`;
+console.log(description);
+
+// Decision Making
+if (population > 33) {
+  console.log(`${country}'s population is above average`);
+} else {
+  console.log(`${country}'s population is ${33 - population} below average`);
+}
