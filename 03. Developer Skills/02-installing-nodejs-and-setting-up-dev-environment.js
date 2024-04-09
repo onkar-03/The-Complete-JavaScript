2b2b1766c3bf524fb654a17f3dc4483f037c5a6e
Onkar Patel <onkarpatel10@gmail.com>
2024-04-09 22:55:13 +0530

    Learned about how to Code (Theory Lecture) & Renamed Files lecture wise

---
+++ 03. Developer Skills/02-installing-nodejs-and-setting-up-dev-environment.js
@@ -0,0 +1,19 @@
+// ^ Installing Node.js & setting up Dev Environment : -------------------------
+
+/*
+ 1. Using VS Code Extension : 'Liver Server'
+     -> Go to Extensions and Download the Live Serer Extension
+*/
+
+/*
+ 2. Installing Node.js & setting up : 
+     -> Install Node.js from Chrome
+     -> run node -v after installation on terminal
+     -> If we can see the version of Node.js = Successful installation
+*/
+
+/*
+ 3. Installing NPM Package of live-server : 
+     -> Type 'npm install liver-server -g' in Terminal
+     -> Type 'live-server' in Terminal to run live server package and load page
+*/
