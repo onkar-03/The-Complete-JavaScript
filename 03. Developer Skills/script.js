ddf3e3919d6d659bdb77f5b63adef47b5b0a5e5e
Onkar Patel <onkarpatel10@gmail.com>
2024-04-09 17:27:14 +0530

    Installing Node.js & Setting up Dev Environment

--- 03. Developer Skills/script.js
+++ 03. Developer Skills/script.js
@@ -1,3 +1,7 @@
+// & ------------------------------> Lecture 01 <-------------------------------
+
+// ^ Setting up Prettier & User Snippets : -----------------------------------
+
 // Using strict mode in all scripts now!
 // Changed the default "" for all strings and scripts of Js to '' in Prettier
 'use strict';
@@ -11,3 +15,20 @@ const calcAge = birthYear => {
 
 // After setting Snippets 'cl' works for console.log()
 console.log(x);
+
+// & ------------------------------> Lecture 02 <-------------------------------
+
+// ^ Installing Node.js & setting up Dev Environment : -------------------------
+
+/* 
+ 1. Using VS Code Extension : Liver Server
+
+ 2. Installing Node.js & setting up : 
+      -> Install Node.js from Chrome
+      -> run node -v after installation on terminal
+      -> If we can see the version of Node.js = Successful installation
+ 
+ 3. Installing NPM Package of live-server : 
+     -> Type 'npm install liver-server -g' in Terminal
+     -> Type 'live-server' in Terminal to run live server package
+*/
