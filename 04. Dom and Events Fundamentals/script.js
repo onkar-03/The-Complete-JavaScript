31d2aa8cc73a1dc93669a60590a45163efbaae13
Onkar Patel <onkarpatel10@gmail.com>
2024-04-10 17:42:28 +0530

    Learned about selecting & manipulating  elements

--- 04. Dom and Events Fundamentals/script.js
+++ 04. Dom and Events Fundamentals/script.js
@@ -1,21 +1,33 @@
-<<<<<<< HEAD
 'use strict';
 
-// -------- DOM Selection & Content Access :
+// -------------- DOM Selection & Content Access :
 
 // A) Query Selector :
 // - A method in JavaScript that allows you to select elements from the DOM (Document Object Model) using CSS selector syntax
 // - It returns the first element within the document that matches the specified selector, or null if no matches are found.
 
 // B) Text Content :
-// - To select the text content of an element with a class name .message, you can use the querySelector() method to select the element by its class name
+// - To select the text content of an element with a class name, you can use the querySelector() method to select the element by its class name
 // - Then access its text  using .textContent property
 
-//Selecting Elements
-console.log(document.querySelector('.message'));
+// Selecting Elements
+// console.log(document.querySelector('.message'));
 
 // Access Content of Element
-console.log(document.querySelector('.message').textContent);
-=======
+console.log(document.querySelector('.message').textContent); // Start Guessing
 
->>>>>>> 9a51842fd9c9b99968b723fcf5744a3925a1d8c7
+// -------------- DOM Manipulation - Selecting & Manipulating Elements :
+
+// Selecting Elements and Changing Content
+document.querySelector('.message').textContent = '🥳 Correct Number';
+
+// Printing
+console.log(document.querySelector('.message').textContent); // 🥳 Correct Number
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+// - To set a value to the selected element with a class name, you can use the querySelector() method to select the element by its class name
+// - Then set its value using the .value property
+document.querySelector('.guess').value = 10;
+console.log(document.querySelector('.guess').value);
