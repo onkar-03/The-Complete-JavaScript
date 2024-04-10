0bf134ec9b8eb79796b7f9f8baac7041ece44399
Onkar Patel <onkarpatel10@gmail.com>
2024-04-10 17:23:13 +0530

    Learned how to select Elements using Js :
    - How to select elements using CSS Style Syntax - document.querySelector
     - How to access content of a particular element - .textContent

---
+++ 04. Dom and Events Fundamentals/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// -------- DOM Selection & Content Access :
+
+// A) Query Selector :
+// - A method in JavaScript that allows you to select elements from the DOM (Document Object Model) using CSS selector syntax
+// - It returns the first element within the document that matches the specified selector, or null if no matches are found.
+
+// B) Text Content :
+// - To select the text content of an element with a class name .message, you can use the querySelector() method to select the element by its class name
+// - Then access its text  using .textContent property
+
+//Selecting Elements
+console.log(document.querySelector('.message'));
+
+// Access Content of Element
+console.log(document.querySelector('.message').textContent);
