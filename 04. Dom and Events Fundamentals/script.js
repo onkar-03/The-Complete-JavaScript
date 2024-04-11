2dc98881254dd51b2cfe1b36f00f33ce9793bcf6
Onkar Patel <onkarpatel10@gmail.com>
2024-04-11 23:54:00 +0530

    Completed the Coding Callenge #1 of game reset

--- 04. Dom and Events Fundamentals/script.js
+++ 04. Dom and Events Fundamentals/script.js
@@ -27,7 +27,7 @@
 // - (+1) at the End to make the range from 0 (inclusive) to 1 (inclusive)
 // - (*) Number means we want a Number between 0 (inclusive) and Number (inclusive)
 // - Math.trunc() used to provide only Integer Part of the Number
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 //Testing
 console.log(secretNumber);
@@ -35,6 +35,7 @@ console.log(secretNumber);
 // ------------------ Default Score :
 let score = 20;
 
+// ------------------ Game Logic :
 // Steps :
 // - Select the Element by its Class / Id name on which we want to add an event
 // - Use the .addEventListener() method to add an event
@@ -64,6 +65,9 @@ document.querySelector('.check').addEventListener('click', function () {
     // Width of Number
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
+
+    // Set High Score
+    document.querySelector('.highscore').textContent = score;
   }
   // When guess is too high
   else if (guess > secretNumber) {
@@ -95,4 +99,42 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
+
+// ------------------ Game Reset :
+document.querySelector('.again').addEventListener('click', function () {
+  // New Random Number
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // Reset Score
+  score = 20;
+  document.querySelector('.score').textContent = score;
+
+  // Reset Text
+  document.querySelector('.message').textContent = 'Start guessing...';
+
+  // Reset Color
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  // Reset Width
+  document.querySelector('.number').style.width = '15rem';
+
+  // Reset Number
+  document.querySelector('.number').textContent = '?';
+
+  // Box Blank
+  document.querySelector('.guess').value = '';
 });
