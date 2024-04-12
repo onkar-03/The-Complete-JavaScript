11a12e965a1c6da33f5ffbc108610bd7f518dc72
Onkar Patel <onkarpatel10@gmail.com>
2024-04-13 00:08:12 +0530

    Learned about Wroking with classes
    - Learned about .classsList to add remove etc.. classes as per our
      requirement
    - Learned about difference in writing just functionName vs
      FunctionName()

--- 06. Project #2 Modal/script.js
+++ 06. Project #2 Modal/script.js
@@ -15,8 +15,37 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
 //Printing to console
-console.log(btnOpenModal);
+// console.log(btnOpenModal);
 
-// Printing each elements content to console
-for (let i = 0; i < btnOpenModal.length; i++)
-  console.log(btnOpenModal[i].textContent);
+// Function created to add hidden class whenever we want to hide the popup
+const closeModal = function () {
+  //Hide them again
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// Selecting Each Element
+for (let i = 0; i < btnOpenModal.length; i++) {
+  // btnOpenModal[i] : is the current element selected
+  btnOpenModal[i].addEventListener('click', function () {
+    // console.log('Button Clicked');
+
+    // => Purpose :
+    // - On click we want to display the hidden popup content & overlay
+    // - This means we want to unhide the content & overlay already hidden
+    // - This means we want to remove the 'hidden' class from the element so as to make it visible
+    // - .classList property is used to access the classes of an HTML element To add or remove classes etc ...
+    // - We dont use the . / # here to access classes or ids its only for the selector
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+  // Closing popup on clicking the cross : btnCloseModal
+  btnCloseModal.addEventListener('click', closeModal);
+
+  // Closing the Popup on clicking outside the popup on the overlay
+  // - Important : writing only closeModal & not closeModal()
+  // - closeModal() means that we call teh function as soon as Js loads
+  // - closeModal means we call the function only when the click happens
+  overlay.addEventListener('click', closeModal);
+}
