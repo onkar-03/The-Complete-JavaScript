c5cdb76621af5425e3187d1fd90f3943ec673a1a
Onkar Patel <onkarpatel10@gmail.com>
2024-04-13 20:48:13 +0530

    Added the Toggling/Switching Player Functionality

--- 07. Project #3 Pig Game/script.js
+++ 07. Project #3 Pig Game/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -14,7 +16,12 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = '0';
 score1El.textContent = '0';
 diceEl.classList.add('hidden');
+
 let currentScore = 0;
+let activePlayer = 0;
+
+// Scores of both players
+const scores = [0, 0];
 
 // Rolling Dice Functionality
 btnRoll.addEventListener('click', function () {
@@ -32,11 +39,22 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // - 5. If not 1 add dice value to the current score
     currentScore += dice;
-    current0El.textContent = currentScore; // Change Later
-  } else {
-    // - 5. If a 1 then reset the Score else add to the current score
-    currentScore = '0';
-  }
+    // current0El.textContent = currentScore; // Change Later
 
-  // - 5. If a 1 then reset the Score else add to the current score
+    // Increase the Player score of the Active Player 0 / 1
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // - 5. If we switch players we reset the current score of the previous player to 0
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    // - 5. Now we switch the Player as soon as the Dice rolls 1
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    // - 5. Setting teh Current Score to start condition
+    currentScore = 0;
+
+    // Change the Background colors for Active and not Active Players
+    // - Toggle adds class mentioned if not there and removes class if already present
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
 });
