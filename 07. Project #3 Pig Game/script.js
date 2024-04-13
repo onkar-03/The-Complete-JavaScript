447b8d6a4925d56eda9181f6da47c56d7224f3bc
Onkar Patel <onkarpatel10@gmail.com>
2024-04-13 21:14:04 +0530

    Holding curretn Score and Playing State of the Game

--- 07. Project #3 Pig Game/script.js
+++ 07. Project #3 Pig Game/script.js
@@ -19,42 +19,84 @@ diceEl.classList.add('hidden');
 
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
-// Scores of both players
+// Scores of both players scores in an array
 const scores = [0, 0];
+
+const switchPlayer = function () {
+  // - If we switch players we reset the current score of the previous player to 0
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  // - Now we switch the Player as soon as the Dice rolls 1
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  // - Setting teh Current Score to start condition
+  currentScore = 0;
+
+  // Change the Background colors for Active and not Active Players
+  // - Toggle adds class mentioned if not there and removes class if already present
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // Rolling Dice Functionality
 btnRoll.addEventListener('click', function () {
-  // -------------- STEPS :
-  // - 1. Generate a Random Number
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  // If Playing State active only then do these
+  if (playing) {
+    // -------------- STEPS :
+    // - 1. Generate a Random Number
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // - 2. Remove the Dice hidden class and make it visible
-  diceEl.classList.remove('hidden');
+    // - 2. Remove the Dice hidden class and make it visible
+    diceEl.classList.remove('hidden');
 
-  // - 3. Display the Dice Image According to the Number
-  diceEl.src = `dice-${dice}.png`;
+    // - 3. Display the Dice Image According to the Number
+    diceEl.src = `dice-${dice}.png`;
 
-  // - 4. Check if the Number is 1 or Not
-  if (dice !== 1) {
-    // - 5. If not 1 add dice value to the current score
-    currentScore += dice;
-    // current0El.textContent = currentScore; // Change Later
+    // - 4. Check if the Number is 1 or Not
+    if (dice !== 1) {
+      // - 5. If not 1 add dice value to the current score
+      currentScore += dice;
+      // current0El.textContent = currentScore; // Change Later
 
-    // Increase the Player score of the Active Player 0 / 1
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
+      // Increase the Player score of the Active Player 0 / 1
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switching
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  // If still playing only then do this
+  if (playing) {
+    // Add the Current score to existing Score of the Player
+    scores[activePlayer] += currentScore;
+
+    // Change the Score on the screen
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+  }
+
+  // If score ?= 100 then That Player wins else we switch player
+  if (scores[activePlayer] >= 100) {
+    // Finish Game
+
+    // No More Continuation of Game
+    playing = false;
+
+    // Declare the Player as Winner
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+
+    // Remove Active Player as Game Over
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
   } else {
-    // - 5. If we switch players we reset the current score of the previous player to 0
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    // - 5. Now we switch the Player as soon as the Dice rolls 1
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    // - 5. Setting teh Current Score to start condition
-    currentScore = 0;
-
-    // Change the Background colors for Active and not Active Players
-    // - Toggle adds class mentioned if not there and removes class if already present
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    // Switching
+    switchPlayer();
   }
 });
