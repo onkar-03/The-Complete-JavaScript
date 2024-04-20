eb3ab43dd47f2cceadf12d2b9b6df0bebe0f880d
Onkar Patel <onkarpatel10@gmail.com>
2024-04-20 23:41:35 +0530

    Completed Challenge #1

---
+++ 09-Data-Structures-Operators/Challenge/challenge-1.js
@@ -0,0 +1,128 @@
+'use strict';
+/* 
+Coding Challenge #1
+We're building a football betting app (soccer for my American friends )!
+Suppose we get data from a web service about a certain game ('game' variable on
+next page). In this challenge we're gonna work with that data.
+
+Your tasks:
+1. Create one player array for each team (variables 'players1' and
+'players2')
+2. The first player in any player array is the goalkeeper and the others are field
+players. For Bayern Munich (team 1) create one variable ('gk') with the
+goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+field players
+3. Create an array 'allPlayers' containing all players of both teams (22
+players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+new array ('players1Final') containing all the original team1 players plus
+'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called
+'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player
+names (not an array) and prints each of them to the console, along with the
+number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which
+team is more likely to win, without using an if/else statement or the ternary
+operator.
+
+Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
+Then, call the function again with players from game.scored 
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// Task 1 :
+// - Destructuring Array
+const [players1, players2] = game.players;
+
+console.log(players1);
+console.log(players2);
+
+// Task 2 :
+// - Using Rest Parameter
+const [gk, ...fieldPlayers] = players1;
+
+console.log(gk);
+console.log(fieldPlayers);
+
+// Task 3 :
+// - merging 2 arrays using spread operator
+const allPlayers = [...players1, ...players2];
+
+console.log(allPlayers);
+
+// Task 4 :
+// - Adding elements to array players1
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+console.log(players1Final);
+
+// Task 5 :
+// - Nested odds object with specific names
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+
+console.log(team1, draw, team2);
+
+// Task 6 :
+// - Print Goals
+// - using the rest operator to take all the arguments under one head
+let printGoals = function (...players) {
+  console.log(`${players.length} goals were scored !!`);
+};
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals('Davies', 'Muller');
+
+// - Printing Number of goals form the scored array
+// - Taking teh Individual values out form the array using spread operator
+printGoals(...game.scored);
+
+// Task 7 :
+// - Lower ODD team Wins
+// - Using logical && as we want the cl to be evaluated when the expression is true
+// - If we use the OR then the expression ShortCircuits when the comparison gets true wand we wont be able to execute the console statement
+team1 < team2 && console.log(`Team 1 is more likely to Win !!`);
+team1 > team2 && console.log(`Team 2 is more likely to Win !!`);
