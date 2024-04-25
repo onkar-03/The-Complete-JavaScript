024fd49591b605d0fe847ed7a4c044d92933c89f
Onkar Patel <onkarpatel10@gmail.com>
2024-04-25 22:31:33 +0530

    Some more Pratice on String Methods

--- 09-Data-Structures-Operators/Challenge/challenge-4.js
+++ 09-Data-Structures-Operators/Challenge/challenge-4.js
@@ -22,7 +22,7 @@ delayedDeparture    ✅✅✅✅✅
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 document.querySelector('button').addEventListener('click', function () {
-  // - Fetching teh Value Entered in Textarea
+  // - Fetching the Value Entered in Textarea
   const text = document.querySelector('textarea').value;
   console.log(text);
 
@@ -45,27 +45,3 @@ document.querySelector('button').addEventListener('click', function () {
 
   // camelCase(text);
 });
-
-function camelCase(str) {
-  // - Remove all spaces from the string
-  const freshString = str.trim();
-  // console.log(freshString);
-
-  // - Now split them using '_' separator
-  const splitStrings = freshString.split('_');
-  // console.log(splitStrings);
-
-  // - For camelCase conversion
-  // - First Fetch the Second Word of the String Stored in the split array
-  // - Then Convert its first character to Uppercase & join the rest of the string
-  // - Now Join the 2 strings of Split array'
-  const final =
-    splitStrings[0] +
-    splitStrings[1][0].toUpperCase() +
-    splitStrings[1].slice(1);
-
-  console.log(final);
-}
-
-// camelCase('  under_score ');
-// camelCase('Some_Variable ');
