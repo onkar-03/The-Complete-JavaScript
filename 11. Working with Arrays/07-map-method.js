7df0ea82bd2b04de76c475091f53d7ef4f553c16
Onkar Patel <onkarpatel10@gmail.com>
2024-05-04 23:54:14 +0530

    Learned about the Map Method on Arrays

---
+++ 11. Working with Arrays/07-map-method.js
@@ -0,0 +1,41 @@
+// --- Map Method :
+// - First the map method is yet another method that we can use to loop over arrays.
+// - So, map is actually similar to the forEach method that we studied before but with the difference that map creates a brand new array based on the original array.
+// -  So essentially the map method takes an array, loops over that array and in each alteration, it applies a callback function that we specify on our code to the current array element, and stores it in a new array
+// -  We say that it maps the values of the original array to a new array and that's why this method is called map.
+// - Also the Map method has access to all three parameters Value, Index & Array
+// - We always return the value we wan th new array to have in the callback function
+
+'use strict';
+
+// --- Using Map:
+// - Array
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// - Conversion to Euro to USD
+const eurToUsd = 1.1;
+
+// - Using arrow function in the callback function
+const movementsToUsd = movements.map(value => {
+  // - Fixed Decimal Parts to 2 places only
+  let number = value * eurToUsd;
+  number = number.toFixed(2);
+
+  // - Returned the calculated value that we want the new Array to have
+  return number;
+});
+
+console.log(movements);
+console.log(movementsToUsd);
+
+// - Using three parameters in Map Method
+// - Using Arrow Function in the callback function
+// - abs() remove the negative part of the value
+const movementsDesc = movements.map(
+  (mov, i, arr) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+
+console.log(movementsDesc);
