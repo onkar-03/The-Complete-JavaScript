1ce3da25f69cdf47baf29cc200e8955ad6d95f4d
Onkar Patel <onkarpatel10@gmail.com>
2024-05-30 00:13:32 +0530

    Completed the Bnakist App Website

--- 14. Advanced DOM/11-efficient-script-loaing-defer-andd-async.js
+++ 14. Advanced DOM/11-efficient-script-loaing-defer-andd-async.js
@@ -11,18 +11,20 @@ Scripts are fetched and executed after the HTML is completely parsed
 
 
 ━ Async ━
-LOADING: Parsing HTML + Script Loading simultaneously → HTML parsing Waiting until script execution completes → Finish Parsing HTML (DOMContentLoaded)
-Fetch Script → Execute
+LOADING: Parsing HTML + Script Loading simultaneously → HTML parsing waits until script execution completes → Finish Parsing HTML (DOMContentLoaded)
 - 2 asynchronous task is executed
 - Scripts are fetched asynchronously and executed immediately
 - Scripts are not guaranteed to execute in order
+- DOMContentLoaded generally waits for all scripts to load and execute completely but not when the scripts are declared asynchronous
 - Use for 3rd-party scripts where order doesn't matter (e.g Google Analytics)
 
-
+ 
 ━ Defer ━
-Parsing HTML + Script is fetched simultaneously without any stops → After complete HTML Parsing Script is executed
+LOADING: Parsing HTML + Script is fetched simultaneously without any stops → After complete HTML Parsing Script is executed
 - No interruption in Parsing HTML
 - Scripts are fetched asynchronously and executed after the HTML is completely parsed
 - Scripts are executed in order
-- This is overall the best solution! Use for your own scrips, and when order matters (e.g. including a library)
+- This is overall the best solution when declaring scripts in the head! Or for scripts, where order of execution matters (e.g. including a library)
 */
+
+// Writing defer / async for script tag in body doesn't make any sense here as the script is loaded only after the HTML Parsing is completed
