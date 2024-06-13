127baa90ee8dbaf07cebbd10a742d88fcd2ba513
Onkar Patel <onkarpatel10@gmail.com>
2024-06-13 23:43:27 +0530

    Learend about Geolocation API & Map URL creation

--- 17. Mapty App/script.js
+++ 17. Mapty App/script.js
@@ -11,3 +11,29 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// --- Geolocation API ---
+// A Browser API, just like Internationalization / Timer etc ...
+// On using this Browser asks to Access our Current Location through a Popup window
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    // Access the Latitude and Longitude of the Current Location
+
+    // Method 1:
+    // const latitude = position.coords.latitude;
+    // const longitude = position.coords.longitude;
+
+    // Method 2:
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    // Create Map URL
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+  },
+  function () {
+    // If we block the Position Access Permission then we get this error Message
+    // We can also ge this error message in case the Geolocation API is unable to access our location ue to other reasons
+    alert(`Could not get current position !!`);
+  }
+);
