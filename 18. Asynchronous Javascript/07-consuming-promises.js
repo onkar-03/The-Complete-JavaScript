2a0b2cd189c6dd0f5b3377da94fd2ce8c89e63f7
Onkar Patel <onkarpatel10@gmail.com>
2024-06-28 23:59:35 +0530

    Consuming Promises

---
+++ 18. Asynchronous Javascript/07-consuming-promises.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////
+// --- 06. Consuming Promises::
+
+//Render Country
+const renderCountry = function (data, className = '') {
+  // Building Card Component
+  const html = `<article class="country ${className}">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>$${(
+              +data.population / 1000000
+            ).toFixed(1)} million</p>
+            <p class="country__row"><span>🗣️</span>${Object.values(
+              data.languages
+            ).join(' ')}</p>
+            <p class="country__row"><span>💰</span>${Object.values(
+              data.currencies
+            )
+              .map(currency => `${currency.name} (${currency.symbol})`)
+              .join(' ')}</p>
+          </div>
+        </article>`;
+
+  // Insert HTML to Page
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+
+  // Style Opacity 1 tio view it
+  countriesContainer.style.opacity = 1;
+};
+
+// Consuming Promise
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      renderCountry(data[0]);
+    });
+};
+
+getCountryData('portugal');
