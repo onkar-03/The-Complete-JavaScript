5762c4bf8aa4b58517378793e06b8ac070eb6580
Onkar Patel <onkarpatel10@gmail.com>
2024-07-04 17:35:20 +0530

    Completed Challenge #3 Part 2

--- 18. Asynchronous Javascript/Challenge/challenge-3.js
+++ 18. Asynchronous Javascript/Challenge/challenge-3.js
@@ -18,31 +18,6 @@ TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn of
 GOOD LUCK 😀
 */
 
-///////////////////////////////////////
-// Coding Challenge #2
-
-/* 
-Build the image loading functionality that I just showed you on the screen.
-
-Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
-
-PART 1
-1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
-
-If this part is too tricky for you, just watch the first part of the solution.
-
-PART 2
-2. Consume the promise using .then and also add an error handler;
-3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
-4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
-5. After the second image has loaded, pause execution for 2 seconds again;
-6. After the 2 seconds have passed, hide the current image.
-
-TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
-
-GOOD LUCK 😀
-*/
-
 // Wait function Promisifying
 const wait = function (seconds) {
   return new Promise(function (resolve) {
@@ -115,3 +90,20 @@ const loadAndPause = async function () {
 };
 
 loadAndPause();
+
+// PART 2
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async img => await createImage(img));
+    // console.log(imgs);
+
+    const imgEl = Promise.all(imgs);
+    // console.log(imgEl);
+
+    imgEl.forEach(img => img.classList.add('parallel'));
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+loadAll(['./img/img-1.jpg', './img/img-2.jpg', './img/img-3.jpg']);
