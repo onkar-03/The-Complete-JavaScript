dcc6192b892048fe56d792bdffd28698e39b1eda
Onkar Patel <onkarpatel10@gmail.com>
2024-07-06 00:13:32 +0530

    Added Detailed Explanation of the Imports & Exports

--- 19. Modern JavaScript Development Modules, Tooling, and Functional/03-importing -and-exporting-es6-modules.js.js
+++ 19. Modern JavaScript Development Modules, Tooling, and Functional/03-importing -and-exporting-es6-modules.js.js
@@ -1,7 +1,11 @@
 ///////////////////////////////////////
 // Exporting and Importing in ES6 Modules
 
-// --- Importing Individual Values from a Module
+// --- Importing Multiple Variables & Renaming Variables during Import
+// For Importing we specify the names of variables we want to import inside the {} using ,
+// We can also rename the Imported Variables as we want using the 'as' keyword here as well
+
+// CODE:
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // addToCart('bread', 5);
 // console.log(price, tq);
@@ -10,18 +14,47 @@ console.log('Importing module');
 // console.log(shippingCost);
 
 // --- Importing All Values from a Module
+// For importing all the variables declared a export in the exporting module we use the *
+// Syntax: import * as objectName from './Path'
+//  ShoppingCart is an object. This object will contain all the exports from the shoppingCart.js
+// To access any variables from Exporting module we use the ObjectName.variableName
+
+// CODE:
 // import * as ShoppingCart from './shoppingCart.js';
 // ShoppingCart.addToCart('bread', 5);
 // console.log(ShoppingCart.totalPrice);
 
 // --- Importing a Default Export into the Module
+// Here for the default import we dont have to write it inside the {}
+// Also we can give it any name we want, and use the same name to perform operations
+
+// CODE:
+// import add from './shoppingCart.js';
+// add('pizza', 2);
+
+// --- Mixing Default and Named Imports
+// WSe can also mix the default and named imports as follows
+// But its not considered a Good Practice as it increases Code COmplexity and reduces Readability
+
+// CODE:
 // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// add('pizza', 2);
 // console.log(price);
 
 // --- Visualizing Import & Export as Live Connection
+
 import add, { cart } from './shoppingCart.js';
 add('pizza', 2);
 add('bread', 5);
 add('apples', 4);
 
 console.log(cart);
+
+// O/P:
+// Array(3): Containing the Three Items Added to the cart
+
+// Explanation:
+// We exported an Empty Cart from ShoppingCart Module and imported it here
+// The imported cart is not a copy but a live connection to the original cart in the shoppingCart.js module. Changes to the original cart are reflected in the imported cart
+// The add function modifies the cart array by pushing new items into it
+// When you log the cart, you see the current state of the cart, reflecting all additions made by the add function
--- 19. Modern JavaScript Development Modules, Tooling, and Functional/shoppingCart.js
+++ 19. Modern JavaScript Development Modules, Tooling, and Functional/shoppingCart.js
@@ -1,9 +1,16 @@
 // Exporting module
 console.log('Exporting module');
 
+// Variables
+// These variables are Private to the Module only unless Exported
 const shippingCost = 10;
 export const cart = [];
 
+// --- Export
+// For exporting we use the 'export' keyword always
+
+// Exporting Individual Variables from Module
+// For this we need to write the 'export' keyword before each of the variables we want to export
 export const addToCart = function (product, quantity) {
   cart.push({ product, quantity });
   console.log(`${quantity} ${product} added to cart`);
@@ -12,8 +19,16 @@ export const addToCart = function (product, quantity) {
 const totalPrice = 237;
 const totalQuantity = 23;
 
+// --- Exporting Multiple Variables & Renaming Variables during Export
+// For exporting multiple variables we need to specify them inside {} using ,
+// We can also rename a variable of the module while exporting using the 'as' keyword
+// Here we export the totalQuantity as tq while exporting
 export { totalPrice, totalQuantity as tq };
 
+// --- Default Exports
+// We use them when we want to export one value per Module
+// We use the default keyword here
+// Here we dont specify any name rather pass a value like the function itself and not the variable addToCart
 export default function (product, quantity) {
   cart.push({ product, quantity });
   console.log(`${quantity} ${product} added to cart`);
