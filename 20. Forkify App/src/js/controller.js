a8230ba41918e041ae1bf81cadcd7576f329b3b9
Onkar Patel <onkarpatel10@gmail.com>
2024-07-13 23:59:49 +0530

    Initial Setup of Forkify App

--- 20. Forkify App/src/js/controller.js
+++ 20. Forkify App/src/js/controller.js
@@ -8,6 +8,29 @@ const timeout = function (s) {
   });
 };
 
+// API
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    // API Request
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+    );
+
+    // Convert to Js Object
+    const data = await res.json();
+
+    if (!res.ok) {
+      throw new Error(`${data.message}(${res.status})`);
+    }
+
+    console.log(res);
+    console.log(data);
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+showRecipe();
